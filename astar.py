3a6d556e602bbe315724570b979860aacedfa12f
Michael Teolis <michaelteolis96@gmail.com>
2021-03-27 14:23:35 -0400

    Fix heuristic 2 and add start of heuristic 1

--- astar.py
+++ astar.py
@@ -19,6 +19,7 @@ class AStar:
         self.search = open(astar_search_path_output, "w")
 
     # number of misplaced tiles from current state to goal state
+    # NOT USED
     def misplaced_score(self):
         misplaced = 0
         for row in range(self.puzzle.len_row()):
@@ -50,16 +51,60 @@ class AStar:
 
         return belongs_on_col
 
-    def swap_top_score(self, current):
+    def swap_top_score_h1(self, current):
+        top = self.puzzle.get_top(current)
+        self.puzzle.swap(current, top)
+        score = self.h1_score()
+        self.puzzle.swap(current, top)
+
+        return score
+
+    def swap_left_score_h1(self, current):
+        left = self.puzzle.get_left(current)
+        self.puzzle.swap(current, left)
+        score = self.h1_score()
+        self.puzzle.swap(current, left)
+
+        return score
+
+    def swap_right_score_h1(self, current):
+        right = self.puzzle.get_right(current)
+        self.puzzle.swap(current, right)
+        score = self.h1_score()
+        self.puzzle.swap(current, right)
+
+        return score
+
+    def swap_bottom_score_h1(self, current):
+        bottom = self.puzzle.get_bottom(current)
+        self.puzzle.swap(current, bottom)
+        score = self.h1_score()
+        self.puzzle.swap(current, bottom)
+
+        return score
+
+    def swap_top_score_h2(self, current):
+        top = self.puzzle.get_top(current)
+        if self.closed[self.puzzle.get_row_of(top)][self.puzzle.get_col_of(top)]:
+            return maxsize
         return abs(self.belongs_on_row_index(current) - self.puzzle.get_row_of(self.puzzle.get_top(current)))
 
-    def swap_bottom_score(self, current):
+    def swap_bottom_score_h2(self, current):
+        bottom = self.puzzle.get_bottom(current)
+        if self.closed[self.puzzle.get_row_of(bottom)][self.puzzle.get_col_of(bottom)]:
+            return maxsize
         return abs(self.belongs_on_row_index(current) - self.puzzle.get_row_of(self.puzzle.get_bottom(current)))
 
-    def swap_left_score(self, current):
+    def swap_left_score_h2(self, current):
+        left = self.puzzle.get_left(current)
+        if self.closed[self.puzzle.get_row_of(left)][self.puzzle.get_col_of(left)]:
+            return maxsize
         return abs(self.belongs_on_col_index(current) - self.puzzle.get_col_of(self.puzzle.get_left(current)))
 
-    def swap_right_score(self, current):
+    def swap_right_score_h2(self, current):
+        right = self.puzzle.get_right(current)
+        if self.closed[self.puzzle.get_row_of(right)][self.puzzle.get_col_of(right)]:
+            return maxsize
         return abs(self.belongs_on_col_index(current) - self.puzzle.get_col_of(self.puzzle.get_right(current)))
 
     # def f_score(self):
@@ -86,20 +131,40 @@ class AStar:
 
         return top, left, right, bottom
 
-    def get_swap_scores(self, current):
+    def get_swap_scores_h1(self, current):
+        swap_top_score = maxsize if self.puzzle.get_top(current) is None else self.swap_top_score_h1(current)
+        swap_left_score = maxsize if self.puzzle.get_left(current) is None else self.swap_left_score_h1(current)
+        swap_right_score = maxsize if self.puzzle.get_right(current) is None else self.swap_right_score_h1(current)
+        swap_bottom_score = maxsize if self.puzzle.get_bottom(current) is None else self.swap_bottom_score_h1(current)
+
+        return swap_top_score, swap_left_score, swap_right_score, swap_bottom_score
+
+    def get_swap_scores_h2(self, current):
         """
         Get the swap scores of the s_puzzle's current position's value.
         Returns in order top, left, right, bottom
         :param current: current position's value
         :return: swap_top_score, swap_left_score, swap_right_score, swap_bottom_score
         """
-        swap_top_score = maxsize if self.puzzle.get_top(current) is None else self.swap_top_score(current)
-        swap_left_score = maxsize if self.puzzle.get_left(current) is None else self.swap_left_score(current)
-        swap_right_score = maxsize if self.puzzle.get_right(current) is None else self.swap_right_score(current)
-        swap_bottom_score = maxsize if self.puzzle.get_bottom(current) is None else self.swap_bottom_score(current)
+        swap_top_score = maxsize if self.puzzle.get_top(current) is None else self.swap_top_score_h2(current)
+        swap_left_score = maxsize if self.puzzle.get_left(current) is None else self.swap_left_score_h2(current)
+        swap_right_score = maxsize if self.puzzle.get_right(current) is None else self.swap_right_score_h2(current)
+        swap_bottom_score = maxsize if self.puzzle.get_bottom(current) is None else self.swap_bottom_score_h2(current)
 
         return swap_top_score, swap_left_score, swap_right_score, swap_bottom_score
 
+    def manhattan_distance(self, current):
+        return abs(self.belongs_on_row_index(current) - self.puzzle.get_row_of(current)) + abs(
+            self.belongs_on_col_index(current) - self.puzzle.get_col_of(current))
+
+    def h1_score(self):
+        score = 0
+        for current in range_inclusive(1, self.puzzle.get_n()):
+            score += self.manhattan_distance(current)
+
+        return score
+
+    # NOT USED
     def get_swap_row_scores(self, current):
         """
         Get the swap top and bottoms scores of the s_puzzle's current position's value.
@@ -107,11 +172,12 @@ class AStar:
         :param current: current position's value
         :return: swap_top_score, swap_bottom_score
         """
-        swap_top_score = maxsize if self.puzzle.get_top(current) is None else self.swap_top_score(current)
-        swap_bottom_score = maxsize if self.puzzle.get_bottom(current) is None else self.swap_bottom_score(current)
+        swap_top_score = maxsize if self.puzzle.get_top(current) is None else self.swap_top_score_h2(current)
+        swap_bottom_score = maxsize if self.puzzle.get_bottom(current) is None else self.swap_bottom_score_h2(current)
 
         return swap_top_score, swap_bottom_score
 
+    # NOT USED
     def get_swap_col_scores(self, current):
         """
         Get the swap left and right scores of the s_puzzle's current position's value.
@@ -119,20 +185,37 @@ class AStar:
         :param current: current position's value
         :return: swap_left_score, swap_right_score
         """
-        swap_left_score = maxsize if self.puzzle.get_left(current) is None else self.swap_left_score(current)
-        swap_right_score = maxsize if self.puzzle.get_right(current) is None else self.swap_right_score(current)
+        swap_left_score = maxsize if self.puzzle.get_left(current) is None else self.swap_left_score_h2(current)
+        swap_right_score = maxsize if self.puzzle.get_right(current) is None else self.swap_right_score_h2(current)
 
         return swap_left_score, swap_right_score
 
-    # def swap_row(self, current):
-
     def reached_goal_state(self):
-        return self.puzzle.s_puzzle is self.puzzle.goal_state
+        is_correct = []
+        for current in range_inclusive(1, self.puzzle.get_n()):
+            if current != self.puzzle.s_puzzle[self.belongs_on_row_index(current)][self.belongs_on_col_index(current)]:
+                is_correct.append(False)
+            else:
+                is_correct.append(True)
 
-    def star_swap(self, current):
+        if is_correct.__contains__(False):
+            return False
+
+        return True
+
+    def star_swap_h1(self, current):
         top, left, right, bottom = self.get_adjacent(current)
         current_row, current_col = self.puzzle.get_index_of(current)
-        swap_top_score, swap_left_score, swap_right_score, swap_bottom_score = self.get_swap_scores(current)
+        swap_top_score, swap_left_score, swap_right_score, swap_bottom_score = self.get_swap_scores_h1(current)
+        if current == self.puzzle.goal_state[current_row][current_col]:
+            self.closed[current_row][current_col] = True
+        elif swap_top_score < (swap_left_score or swap_right_score or swap_bottom_score):
+            self.puzzle.swap(current, top)
+
+    def star_swap_h2(self, current):
+        top, left, right, bottom = self.get_adjacent(current)
+        current_row, current_col = self.puzzle.get_index_of(current)
+        swap_top_score, swap_left_score, swap_right_score, swap_bottom_score = self.get_swap_scores_h2(current)
         if self.belongs_on_current_row(current) and self.belongs_on_current_col(current):
             self.closed[current_row][current_col] = True
         elif self.belongs_on_current_row(current):
@@ -146,14 +229,18 @@ class AStar:
             else:
                 self.puzzle.swap(current, bottom)
         else:
-            if swap_top_score <= swap_bottom_score and swap_left_score <= swap_right_score:
-                if swap_top_score <= swap_left_score:
+            if swap_top_score <= swap_bottom_score:
+                if swap_top_score <= (swap_left_score and swap_right_score):
                     self.puzzle.swap(current, top)
-                else:
+                elif swap_left_score < swap_right_score:
                     self.puzzle.swap(current, left)
+                else:
+                    self.puzzle.swap(current, right)
             else:
-                if swap_bottom_score <= swap_right_score:
+                if swap_bottom_score <= (swap_left_score and swap_right_score):
                     self.puzzle.swap(current, bottom)
+                elif swap_left_score <= swap_right_score:
+                    self.puzzle.swap(current, left)
                 else:
                     self.puzzle.swap(current, right)
 
@@ -165,8 +252,9 @@ class AStar:
             return True
 
         for current in range_inclusive(1, self.puzzle.get_n()):
+            print(f"\n===========\ncurrent: \n{self.puzzle.s_puzzle}\nclosed: \n{self.closed}\n============")
             while not self.closed[self.belongs_on_row_index(current)][self.belongs_on_col_index(current)]:
-                self.star_swap(current)
+                self.star_swap_h2(current)
 
         self.close_files()
 
--- main.py
+++ main.py
@@ -19,9 +19,21 @@ def test(s_puzzle):
     print(f"current: \n{star.puzzle.s_puzzle}")
     print(f"goal: \n{star.puzzle.goal_state}")
     solved = star.solve()
+    print("==== SOLVED ====")
     print(f"current: \n{star.puzzle.s_puzzle}")
     print(f"goal: \n{star.puzzle.goal_state}")
     print(f"solved: {solved}")
+
+    # print(f"h2_score: {star.h1_score()}")
+    # top, left, right = 3, 1, 2
+    # if top <= left and top <= right:
+    #     print("obvs yes")
+    # else:
+    #     print("obvs no")
+    # if top <= (left or right):
+    #     print(f"{top} <= ({left} and {right})\nAMAZING!")
+    # else:
+    #     print(f"nope")
 
 
 if __name__ == "__main__":
